8a76f89699f84de48d2d632e7fd45e4c29803f17
RodrigoGtzRbl <Rgutierrez@protonmail.com>
2024-07-28 16:48:55 +0200

    update index and js
    Index changing the button to a link to show the ajaxInfo div and in script create random examples in the input's values

--- resources/JS/js.js
+++ resources/JS/js.js
@@ -9,23 +9,55 @@ $(document).ready(function () {
     function search(url) {
 
         $.ajax({
-            url: url, 
-            method: 'GET', 
-            dataType: 'json', 
+            url: url,
+            method: 'GET',
+            dataType: 'json',
             success: function (response) {
                 const jsonString = JSON.stringify(response, null, 2);
-               
+
                 $('#ajaxInfo').empty().html(`
                     <div class="container mt-4">
                         <pre>${jsonString}</pre>
                     </div>
                 `);
             },
-            error: function() {
+            error: function () {
                 $('#ajaxInfo').empty().html(`<p>Error, try it later or check if the URL is correct</p>`);
             }
         });
-        
-        
+
+
     }
+
+    function createRandomPlaceholder() {
+        const files = ['GodsAndDeities', 'Heroes', 'MythicalArtifacts', 'MythicalCreatures', 'Myths'];
+        const file = files[Math.floor(Math.random() * files.length)];
+
+        let access;
+        switch (Math.floor(Math.random() * 3)) {
+            case 1:
+                access = 'all';
+                break;
+
+            case 2:
+                access = 'id/7';
+                break;
+
+            case 3:
+                access = 'name/Zeus';
+                break;
+            default:
+                $('#ajaxSearch').attr('value', "search/The Twelve Labors of Hercules");
+                return;
+
+        }
+
+        $('#ajaxSearch').attr('value', `${file}/${access}`);
+    }
+
+
+    createRandomPlaceholder();
+
+
+
 });
